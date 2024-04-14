aec7786dbe86440f87c323b756f5d4324c0883bd
Nanda Lal Das <tikludas01@gmail.com>
2024-04-15 03:40:25 +0530

    added the navbar for mobile and dekstop version and made it responsive

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -2,6 +2,8 @@ import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
 import { Button } from "../ui/button"
+import Navitems from "./Navitems"
+import MobileNav from "./MobileNav"
 
 const Header = () => {
   return (
@@ -15,10 +17,19 @@ const Header = () => {
                    height={38}
                    priority={true}
                     />
+                
             </Link>
+
+            <SignedIn>
+                 <nav className="md:flex-between hidden w-full max-w-xs" >
+                  <Navitems/>
+                </nav>     
+                </SignedIn>    
+
             <div className="flex w-32 justify-end gap-3">
                 <SignedIn>
                     <UserButton afterSignOutUrl="/"/>
+                    <MobileNav/>
                 </SignedIn>
                 <SignedOut>
                     <Button asChild className="rounded-full" size="lg">
