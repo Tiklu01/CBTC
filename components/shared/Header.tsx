eadcb7ce567dfbf2b345803c38e77e5ccc8f7353
Nanda Lal Das <tikludas01@gmail.com>
2024-04-15 03:05:23 +0530

    The authentication is finally functional

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,4 +1,4 @@
-import { SignedOut } from "@clerk/nextjs"
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
 import { Button } from "../ui/button"
@@ -14,15 +14,20 @@ const Header = () => {
                   width={128}
                    height={38}
                    priority={true}
-                   
                     />
             </Link>
             <div className="flex w-32 justify-end gap-3">
-            <SignedOut>
-                <Button asChild className="rounded-full" size="lg">
-                    <Link href="/sign-in">Login</Link>
-                </Button>
-            </SignedOut>
+                <SignedIn>
+                    <UserButton afterSignOutUrl="/"/>
+                </SignedIn>
+                <SignedOut>
+                    <Button asChild className="rounded-full" size="lg">
+                <Link href="/sign-in">
+                Login
+                </Link>
+                 </Button>
+                 </SignedOut>
+
             </div>
         </div>
      </header>
