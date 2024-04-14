852b0efdf2aa472a2c40a557f0c791017869f40e
Nanda Lal Das <tikludas01@gmail.com>
2024-04-14 21:54:06 +0530

    installed uploadthing package to handle file uploads

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
+      <h1 className='text-4xl'>EventPlanner360</h1>
       <Button variant="destructive">Hello</Button>
     </main>
   )
