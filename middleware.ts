63f3bd2e7985e179bf46ddfa74dfefa126fda4ab
Nanda Lal Das <tikludas01@gmail.com>
2024-04-15 23:20:03 +0530

    added "/assets/images/dotted-pattern.png" to public routes in moddleware to load images even while signed out

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,8 @@ export default authMiddleware({
         'events/:id',
         '/api/webhook/clerk',
         '/api/webhook/stripe',
-        '/api/uploadthing'
+        '/api/uploadthing',
+        "/assets/images/dotted-pattern.png"
     ],
     ignoredRoutes:[
         '/api/webhook/clerk',
