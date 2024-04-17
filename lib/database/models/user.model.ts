656c1f5008cf51bf7d4d2051225724a6edb949e3
Nanda Lal Das <tikludas01@gmail.com>
2024-04-17 11:28:40 +0530

    made changes to user.model.ts

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -3,11 +3,11 @@ import { Schema, model, models } from "mongoose";
 const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
+  username: { type: String, required: false, default: null, unique: true },
   firstName: { type: String, required: true },
-  lastName: {type: String, required: true },
+  lastName: { type: String, required: false, default: null },
   photo: { type: String, required: true },
-})
+});
 
 const User = models.User || model('User', UserSchema);
 
