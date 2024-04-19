c87bef8c8580c6f06d747309b67384ddb03964e1
Nanda Lal Das <tikludas01@gmail.com>
2024-04-19 19:18:59 +0530

    FileUploader.tsx created and Textarea section is made the FileUploader code will be developed next

---
+++ components/shared/FileUploader.tsx
@@ -0,0 +1,16 @@
+import { Dispatch, SetStateAction } from "react"
+
+type FileUploadProps ={
+    imageUrl: string,
+    onFieldChange: (value: string) => void
+    setFiles: Dispatch<SetStateAction<File[]>>
+}
+
+const FileUploader = ({imageUrl, onFieldChange, setFiles}:
+ FileUploadProps) => {
+  return (
+    <div>FileUploader</div>
+  )
+}
+
+export default FileUploader
