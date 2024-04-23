dfc6f9f262da34c2651b037767b50ae58ac00ef6
Nanda Lal Das <tikludas01@gmail.com>
2024-04-23 14:40:20 +0530

    created the collections componen t the card component creation is next

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,19 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
-export default function Home() {
+import Collection from "@/components/shared/Collection";
+import { getAllEvents } from "@/lib/actions/events.actions";
+export default async function Home() {
+
+  const events = await getAllEvents({
+    query: '',
+    category:'',
+    page: 1,
+    limit: 6,
+  })
+
+  
+
   return (
    <>
          <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
@@ -32,6 +44,15 @@ export default function Home() {
       Search
       CatagoryFilter
     </div>
+    <Collection
+    data={events?.data}
+    emptyTitle="No Events Found"
+    emptyStateSubtext="Come back later"
+    collectionType="All_Events"
+    limit={6}
+    page={1}
+    totalPages={2}
+    />
     </section>
    </>
   )
