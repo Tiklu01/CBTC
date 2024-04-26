253fe23740fa69e710149cc4a48f8e99b585e1f9
Nanda Lal Das <tikludas01@gmail.com>
2024-04-26 21:01:36 +0530

    fixed deplyment issue after correcting Typo

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -4,7 +4,7 @@ import { auth } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import { DeleteConfirmation } from './DeleteConfogitrmation'
+import { DeleteConfirmation } from './DeleteConformation'
 
 type CardProps = {
   event: IEvent,
