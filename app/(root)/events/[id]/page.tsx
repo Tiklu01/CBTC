456c73e0d4d2fc61e0b85280a4d07890252c6efa
Nanda Lal Das <tikludas01@gmail.com>
2024-04-28 12:02:53 +0530

    added Checkout Button and initialised stripe setup

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,4 +1,5 @@
 
+import CheckoutButton from '@/components/shared/CheckoutButton';
 import Collection from '@/components/shared/Collection';
 import { getEventById,getRelatedEventsByCategory } from '@/lib/actions/events.actions'; 
 import { formatDateTime } from '@/lib/utils';
@@ -47,7 +48,7 @@ const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) 
             </div>
           </div>
 
-          
+          <CheckoutButton event={event}/>
 
           <div className="flex flex-col gap-5">
             <div className='flex gap-2 md:gap-3'>
